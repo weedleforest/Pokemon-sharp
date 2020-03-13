83a64a812a1e2db0796bc2c48a40e68f425350fb
James Henshall <weedleforest@gmail.com>
2020-03-13 23:21:04 +0000

    Did more work on the pokemon class

--- Pokemon.cs
+++ Pokemon.cs
@@ -3,15 +3,21 @@
 public class Pokemon
 {
 	// array(0) = base array(1) = IV array(2) = EV array(3) = total/max
-	private int Hp();
-	private int attack();
-	private int defence();
-	private int speed();
-	private int special();
+	private int Hp = new int[3];
+	private int attack = new int[3];
+	private int defence = new int[3];
+	private int speed = new int[3];
+	private int special = new int[3];
+	private string levelUpGroup;
+	private int exp;
 	private string name;
 	private int level;
 	private int currentHp;
 	private bool fainted;
+	private bool canEvolve;
+	private int ID;
+	private bool wild;
+	private int baseEXPYeild;
 
 	public Pokemon()
 	{
@@ -28,29 +34,42 @@ public class Pokemon
 		setMaxSpecial;
     }
 
+	public void gainEXP(int enemyEXPyeild, int enemyLevel, bool enemyWild, int numOfPokemon)
+    {
+		if (enemyWild = true)
+        {
+			exp += Math.Ceiling((enemyEXPyeild * enemyLevel) / (7 * numOfPokemon));
+        }
+		else
+        {
+			exp += Math.Ceiling((enemyEXPyeild * enemyLevel * 1.5) / (7 * numOfPokemon)); 
+		}
+		
+    }
+
 	public void setMaxHealth()
     {
-		Hp(3) = ((((Hp(0) + Hp(1)) * 2) + ((Hp(2)^(1/2)) / 4)) / 100 * level) + level + 10;
+		Hp[3] = ((((Hp[0] + Hp[1]) * 2) + ((Hp[2]^(1/2)) / 4)) / 100 * level) + level + 10;
     }
 
 	public void setMaxAttack()
     {
-		attack(3) = ((attack(0) + attack(1)) * 2 + ((attack(2) ^ (1 / 2) / 4)) / 100 * level) + 5; 
+		attack[3] = ((attack[0] + attack[1]) * 2 + ((attack[2] ^ (1 / 2) / 4)) / 100 * level) + 5; 
     }
 
 	public void setMaxDefence()
     {
-		defence(3) = ((defence(0) + defence(1)) * 2 + ((defence(2) ^ (1 / 2) / 4)) / 100 * level) + 5;
+		defence[3] = ((defence[0] + defence[1]) * 2 + ((defence[2] ^ (1 / 2) / 4)) / 100 * level) + 5;
 	}
 
 	public void setMaxSpeed()
     {
-		speed(3) = ((speed(0) + speed(1)) * 2 + ((speed(2) ^ (1 / 2) / 4)) / 100 * level) + 5;
+		speed[3] = ((speed[0] + speed[1]) * 2 + ((speed[2] ^ (1 / 2) / 4)) / 100 * level) + 5;
 	}
 
 	public void setMaxSpecial()
     {
-		special(3) = ((special(0) + special(1)) * 2 + ((special(2) ^ (1 / 2) / 4)) / 100 * level) + 5;
+		special[3] = ((special[0] + special[1]) * 2 + ((special[2] ^ (1 / 2) / 4)) / 100 * level) + 5;
 	}
 
 	public int getBaseHp()
